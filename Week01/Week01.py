06bb6b6d11f739279a84b24d17ea28c3791d2f38
bams-psspss <psaravaneeyawong@myseneca.ca>
2024-09-06 17:40:51 -0400

    Testing if cmd is working

--- Week01/Week01.py
+++ Week01/Week01.py
@@ -1,5 +1,12 @@
+#git add .
+#git commit -m "Comment"
+#git push
+
 def hello():
     name = input("Hello Stranger! What is your name? ");
     print(f"Hello {name}! Nice to meet you!");
 
 hello();
+
+a = 3 + 2
+print (a)
