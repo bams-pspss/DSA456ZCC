68f910916c7472c1cc3ea7845ed50071f47d99e7
bams-psspss <psaravaneeyawong@myseneca.ca>
2024-10-07 11:52:07 -0400

    Something

--- Testing.py
+++ Testing.py
@@ -1,2 +1,14 @@
-print(-9 % 5)
-print(9 % 5)
+#Linked List - Without Node
+class LinkedList:
+    #Constructor
+    def __init__(self):
+        self.front = None
+        self.back = None
+
+#Linked List with Node
+class LinkedList:
+    class Node:
+        def __init__(self, data, next=None, prev=None):
+            self.data = data
+            self.next = next
+            self.prev = prev
